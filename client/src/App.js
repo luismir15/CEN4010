9b7aecf6485f6fe9484210dbbc49711265277483
Eugenio Martin-Carreras <emart364@fiu.edu>
2020-02-07 01:47:48 -0500

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Home from './home'
+import Register from './register'
+import Login from './login'
+import { Route, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
     </div>
   );
 }
