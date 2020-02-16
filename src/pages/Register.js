f57ec542f4f2aeb23d17910d7b4180f9a468957e
Miguel Angel <menendez742@gmail.com>
2020-02-16 17:11:16 -0500

    Major update to structure and home page
    The home page has a library, changed font, and theme

---
+++ src/pages/Register.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+ 
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <p>User profile, settings, account management</p>
+        <ol>
+          <li>etc</li>
+          <li>to be continued</li>
+          <li>etc</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Register;
---
+++ src/server/config/keys.js
@@ -0,0 +1,5 @@
+// Key to acess Mongo db
+module.exports = {
+	mongoURI:
+		'mongodb+srv://geektext:geektext16@geektext-06ylz.mongodb.net/test?retryWrites=true&w=majority'
+};
