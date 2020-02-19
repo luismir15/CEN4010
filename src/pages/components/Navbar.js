a9ca3b2c2f4c57699d02df297ab09c861893c725
luismir15 <luismir15@yahoo.com>
2020-02-19 15:08:44 -0500

    navigation bar component

---
+++ src/pages/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Navitem from './Navitem';
+import SearchField from 'react-search-field';
+
+class Navbar extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            'NavItemActive':''
+        }
+    }
+    activeitem=(x)=>
+    {
+        if(this.state.NavItemActive.length>0){
+            document.getElementById(this.state.NavItemActive).classList.remove('active');
+        }
+        this.setState({'NavItemActive':x},()=>{
+            document.getElementById(this.state.NavItemActive).classList.add('active');
+        });
+    };
+    render() {
+        return (
+            <nav>
+            <ul>
+            <Navitem item="Home" tolink="/"  activec={this.activeitem}></Navitem>
+            <Navitem item="Register" tolink="/register"  activec={this.activeitem}></Navitem>
+            <Navitem item="Login" tolink="/login"  activec={this.activeitem}></Navitem>
+            <Navitem item="Orders" tolink="/orders"  activec={this.activeitem}></Navitem>
+            <Navitem item="Book Details" tolink="/bookdetails"  activec={this.activeitem}></Navitem>
+            <SearchField placeholder='Search item'/>
+            </ul>
+            </nav>
+            )
+        }
+    }
+    
+    export default Navbar
