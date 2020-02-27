85ffadc4582c47cf1849607acbe3e59db040d3fa
Christian Massuh <christianmassuh@Christians-MacBook-Pro.local>
2020-02-26 22:36:10 -0500

    edited the front end for book details

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Button, Image} from 'react-bootstrap';
 
 //useful for when you make calls to the api -- helps open a gateway to collect information
 const api = require('axios');
@@ -11,24 +11,25 @@ class BookDetails extends React.Component {
     super(props);
     this.state = {
       title: "Harry Potter and the Cursed Child - Parts I & II",
-      author: "J. K. Rowling, John Tiffany, Jack Thorne",
+      author: "by " + "J. K. Rowling, John Tiffany, Jack Thorne",
       isbn: "9781338099133",
-      price: "17.99",
+      price: "$17.99",
       publisher: "Scholastic, Inc.",
       releaseDate: "07/31/2016",
       genre: "Fiction",
       description: "The Eighth Story. Nineteen Years Later. Based on an original new story by J.K. Rowling, John Tiffany, and Jack Thorne, a new play by Jack Thorne, Harry Potter and the Cursed Child is the eighth story in the Harry Potter series and the first official Harry Potter story to be...",
       authorBio: "J.K. Rowling is the author of the seven Harry Potter novels, which have sold over 450 million copies and have been translated into 79 languages, and three companion books originally published for charity. She is also the author of The Casual Vacancy, a novel for adults published in 2012, and, under the pseudonym of Robert Galbraith, is the author of the Cormoran Strike crime series. J.K. Rowling is making her screenwriting debut and is a producer on the film Fantastic Beasts and Where to Find Them, a further extension of the Wizarding World, due for release...",
-      image: "https://images-na.ssl-images-amazon.com/images/I/71zWjTSsq1L.jpg"
-
+      image: "https://images-na.ssl-images-amazon.com/images/I/71zWjTSsq1L.jpg",
+      reviews: []
     };
   }
   render() {
+
     return (
       <Container className="details">
         <Row>
-          <Col>
-            <img className="details-img" src={this.state.image} />
+          <Col className="details-img-col">
+            <Image className="details-img" src={this.state.image} fluid/>
           </Col>
 
           <Col className="details-Basics">
@@ -48,16 +49,35 @@ class BookDetails extends React.Component {
               <li>  ISBN: {this.state.isbn} </li>
               <li>  Publisher: {this.state.publisher} </li>
               <li>  Release Date: {this.state.releaseDate}</li>
-              <li>  Genre: {this.state.Genre}</li>
+              <li>  Genre: {this.state.genre}</li>
             </ul>
+
+            <Row className="details-buttons">
+                <Col className="addtoCartBtn" xs={6}>
+                  <Button variant="primary" size="sm" block>ADD TO CART</Button>
+                </Col>
+
+                <Col className="addtoWishlistBtn" xs={6}>
+                  <Button variant="light" size="sm" block>ADD TO WISHLIST</Button>
+                </Col>
+            </Row>
 
           </Col>
 
         </Row>
 
         <Row className="details-description">
-          <h3>Overview</h3>
+          <div className="centering">
+            <h3>Overview</h3>
+          </div>
           <p>{this.state.description}</p>
+        </Row>
+
+        <Row className="customer-reviews">
+            <h3>Customer Reviews</h3>
+          
+
+
         </Row>
 
         <Row className="details-authorBio">
