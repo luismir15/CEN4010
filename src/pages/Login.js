251fa7294224b4f28e251e9284714bda51af8bc0
Eugenio Martin-Carreras <emart364@fiu.edu>
2020-02-28 17:31:32 -0500

    registration and login pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css';
 
 class Login extends Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
-
-        <p>This will be the user login page <span role="img"> 😍</span> :P</p>
-        <li>Can be merged with register/ sign up</li>
-
-        <ol>
-          <input onchange={this.props.handleSearch} type="text" />
-          <form onSubmit={this.props.searchBook} action="">
-
-            <input onchange={this.props.handleSearch} type="text" />
-            <button type="submit">Submit</button>
-          </form>
-        </ol>
-
-      </div>
+        <h1 id="login">Login</h1>
+        <div class='registerlink'>
+          <Link to="/Register" id='registration'>Don't have an account? Register here!</Link>
+        </div>
+        <br />
+        <form id="login">
+          <label>
+            Email:
+          <br />
+            <input
+              type="email"
+              id="reg"
+              name="email"
+              placeholder="jon.snow@thewall.com"
+              required
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Password:
+          <br />
+            <input
+              type="password"
+              id="reg"
+              name="password"
+              minLength="8"
+              required
+            />
+          </label>
+          <br />
+          <br />
+          <input type="submit" class="submit" value="Login" />
+        </form>
+      </div >
     );
   }
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,19 +1,81 @@
-import React, { Component } from "react";
- 
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Register.css';
+
 class Register extends Component {
   render() {
     return (
       <div>
-        <h1>Register</h1>
-        <p>User profile, settings, account management</p>
-        <ol>
-          <li>etc</li>
-          <li>to be continued</li>
-          <li>etc</li>
-        </ol>
-      </div>
+        <h1 id="registration">Register</h1>
+        <div class='loginlink'>
+          <Link to="/Login" id='registration'>Already registered? Login here!</Link>
+        </div>
+        <br />
+        <form id="registration">
+          <label>
+            First Name:
+						<br />
+            <input
+              type="text"
+              id="reg"
+              name="firstname"
+              placeholder="Jon"
+              required
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Last Name:
+						<br />
+            <input
+              type="text"
+              id="reg"
+              name="lastname"
+              placeholder="Snow"
+              required
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Email:
+						<br />
+            <input
+              type="email"
+              id="reg"
+              name="email"
+              placeholder="jon.snow@thewall.com"
+              required
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Password:
+						<br />
+            <input
+              type="password"
+              id="reg"
+              name="password"
+              minLength="8"
+              required
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            Confirm Password:
+						<br />
+            <input type="password" id="reg" name="password" required />
+          </label>
+          <br />
+          <br />
+          <input type="submit" class="submit" value="Register" />
+        </form>
+      </div >
     );
   }
 }
- 
+
 export default Register;
