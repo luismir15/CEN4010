949b0221c562b4922798a96fe9a8692b0e0ab934
cmend017 <50390641+cmend017@users.noreply.github.com>
2020-03-01 00:46:48 -0500

    carlos commit shopcart

--- src/pages/ShopCart.js
+++ src/pages/ShopCart.js
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+//import { Container } from "react-bootstrap/lib/Tab";
+import {Container, Row, Col, Button, Image} from 'react-bootstrap';
  
-class ShopCart extends Component {
+class ShopCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Harry Potter and the Cursed Child - Parts I & II",
+      price: "17.99"
+
+    };
+  }
+  
+  
+  
+  
+  
+  
   render() {
     return (
+      <Container className="details">
+
+<Row className="details-buttons">
+                <Col className="addtoCartBtn" xs={6}>
+                  <Button variant="primary" size="sm" block>ADD TO CART</Button>
+                </Col>
+
+                <Col className="addtoWishlistBtn" xs={6}>
+                  <Button variant="light" size="sm" block>ADD TO WISHLIST</Button>
+                </Col>
+            </Row>
+
+    <Row>
       <div>
         <h1>Shopping Cart</h1>
-        <p>More to be worked on</p>
+        <p> image test</p>
         <ol>
           <li>Order list</li>
           <li>Favorites</li>
@@ -13,6 +42,8 @@ class ShopCart extends Component {
           <li>etc</li>
         </ol>
       </div>
+      </Row>
+      </Container>
     );
   }
 }
