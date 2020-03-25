dd2f078a234309ad84fa64e227a966b991813cd1
Eugenio Martin-Carreras <emart364@fiu.edu>
2020-03-25 14:12:44 -0400

    added some code for user update

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,6 +5,7 @@ import './Profile.css';
 const DEFAULT = 'DEFAULT';
 const ERROR = 'ERROR';
 const AUTHED = 'AUTHED';
+const UPDATE = 'UPDATE';
 
 const Profile = () => {
 	const [state, setState] = React.useState(DEFAULT);
@@ -51,6 +52,8 @@ const Profile = () => {
 			);
 		case AUTHED:
 			return <AuthedProfile user={user} />;
+		case UPDATE:
+			return <ProfileForm user={user} />;
 		case ERROR:
 			return (
 				<div>
@@ -77,6 +80,7 @@ const logout = async e => {
 const AuthedProfile = ({ user }) => (
 	<div>
 		<input type="submit" onClick={logout} class="submit" value="Logout" />
+		<input type="submit" onClick={} class="submit" value="Update" />
 		<h1>Name</h1>
 		<h3>{user?.name}</h3>
 		<h1>User ID</h1>
@@ -89,5 +93,97 @@ const AuthedProfile = ({ user }) => (
 		<h3>{user?.nickname}</h3>
 	</div>
 );
+
+const ProfileForm = ({ user }) => {
+	const handleSubmit = async e => {
+		const loggedIn = localStorage.getItem('userId');
+		axios.put(`http://localhost:3002/api/users?userId=${loggedIn}`, input);
+	};
+
+	const [input, setInput] = React.useState(user);
+
+	return (
+		<div>
+			<form id="update">
+				<label>
+					Full Name:
+					<br />
+					<input
+						type="text"
+						onChange={e => setInput({ ...input, name: e.target.value })}
+						id="reg"
+						name="name"
+						placeholder="Jon Snow"
+						required
+					/>
+				</label>
+				<br />
+				<br />
+				<label>
+					Email:
+					<br />
+					<input
+						type="email"
+						onChange={e => setInput({ ...input, email: e.target.value })}
+						id="reg"
+						name="email"
+						placeholder="jon.snow@thewall.com"
+						required
+					/>
+				</label>
+				<br />
+				<br />
+				<label>
+					Password:
+					<br />
+					<input
+						type="password"
+						onChange={e => setInput({ ...input, password: e.target.value })}
+						id="reg"
+						name="password"
+						minLength="8"
+						required
+					/>
+				</label>
+				<br />
+				<br />
+				<label>
+					Home Address:
+					<br />
+					<input
+						type="text"
+						onChange={e => setInput({ ...input, homeAddress: e.target.value })}
+						id="reg"
+						name="homeAddress"
+						placeholder="1234 The Wall"
+						required
+					/>
+				</label>
+				<br />
+				<br />
+				<label>
+					Nickname:
+					<br />
+					<input
+						type="text"
+						onChange={e => setInput({ ...input, nickname: e.target.value })}
+						id="reg"
+						name="nickname"
+						placeholder="Jonny"
+						required
+					/>
+				</label>
+				<br />
+				<br />
+				<input
+					type="submit"
+					onClick={handleSubmit}
+					class="submit"
+					value="Confirm"
+				/>
+			</form>
+		</div>
+	);
+};
 
 export default Profile;
