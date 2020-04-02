66f785916046e3bedb357479c0f0e27eb4730c85
Miguel Menendez <miguel@cogitocode.org>
2020-04-02 13:12:56 -0400

    merged branches

---
+++ .history/client/src/pages/Home_20200402023426.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import axios from 'axios';
+
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			books: []
+		};
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:3002/api/books').then(res => {
+			this.setState({
+				books: res.data
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<ul className="flexbox-container">
+					{this.state.books.map(book => (
+						<li>
+							<NavLink
+								to={`/Book/${book._id}`}
+								style={{ textDecoration: 'none' }}
+							>
+								<div className="book-wrapper">
+									<div className="flexbox-item flexbox-item-3">
+										<img className="fit-container" src={book.thumbnailUrl} />
+									</div>
+									<h4>{book.title}</h4>
+									<p>by {book.authors}</p>
+								</div>
+							</NavLink>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default Home;
