129cbdcd1a1a28d740aa79a99e2e199cd7e9f387
miguel <menendez742@gmail.com>
2020-04-09 15:42:58 -0400

    wishlist model

--- server/models/Wishlist.js
+++ server/models/Wishlist.js
@@ -1,52 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Create schema
-const Wishlist = new Schema({
-	title: {
-		type: String,
-		required: true
-	},
-	isbn: {
-		type: String,
-		required: true
-	},
-	thumbnailUrl: {
-		type: String,
-		required: true
-	},
-	shortDescription: {
-		type: String,
-		required: true
-	},
-	status: {
-		type: String,
-		required: true
-	},
-	authors: {
-		type: String,
-		required: true
-	},
-	categories: {
-		type: String,
-		required: true
-	},
-	genre: {
-		type: String,
-		required: true
-	},
-	price: {
-		type: Number,
-		required: true
-	},
-	publisher: {
-		type: String,
-		required: true
-	},
-	rating: {
-		type: Number,
-		required: true
-	}
+//new wishlist models
+const WishListSchema = new Schema({
+  userId: { type: String, required: true },
+  products: [{}],
 });
 
-module.exports = mongoose.model('wishlist', Wishlist);
+module.exports = mongoose.model("WishList", WishListSchema);
