9be2ab33b5b62854d59ad8c98cb21d67b87f8c92
Jessica <jmarc107@fiu.edu>
2020-04-10 21:59:15 -0400

    comment components

---
+++ client/src/components/Comments/Comment.js
@@ -0,0 +1,35 @@
+import React from "react";
+import StarRatings from "react-star-ratings";
+export default function Comment(props) {
+  const { name, text, rating, posted } = props.comment;
+  return (
+    <>
+      <div class="col-md-3 text-center">
+        <img
+          class="rounded-circle reviewer"
+          src="http://cdn.onlinewebfonts.com/svg/img_534267.png"
+        />
+        <div class="caption">
+          <small>
+            by <a href="#Miguel">{name}</a>
+          </small>
+        </div>
+      </div>
+      <div class="col-md-9">
+        {/* <h3>Greatest book I've read</h3> */}
+        <div class="ratebox text-center" data-id="0" data-rating="5"></div>
+        <p>
+          <StarRatings
+            style={{ display: "block" }}
+            starRatedColor="#FFCC00"
+            starDimension="25px"
+            rating={rating}
+            numberOfStars={5}
+          />
+        </p>
+        <p class="review-text">{text}</p>
+        <small class="review-date">{new Date(posted).toDateString()}</small>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/Comments/CommentList.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
+import Comment from "./Comment";
+export default class CommentList extends Component {
+  render() {
+    const { comments } = this.props;
+    return (
+      <div className="reviews">
+        {comments.map((comment) => (
+          <Row className="mb-5 mt-5 blockquote review-item">
+            <Col lg={"12"} md={"12"} xs={"12"}>
+              <Comment comment={comment} />
+            </Col>
+          </Row>
+        ))}
+      </div>
+    );
+  }
+}
